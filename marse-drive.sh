2eedd8e11459c6a30006b0900f0f48b9d5a2aebe
EKO HENDRATNO <eko.hendratno@gmail.com>
2025-03-09 07:11:55 +0700

    Create marse-drive.sh

---
+++ marse-drive.sh
@@ -0,0 +1,5 @@
+curl -L --output cloudflared.deb https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64.deb && 
+
+sudo dpkg -i cloudflared.deb && 
+
+sudo cloudflared service install eyJhIjoiYWFiMDFmNjEyNDQ4MjJhODYyOTZhMDRlYmU0ODEyM2QiLCJ0IjoiZDE5MTI2NjctMmZhMC00OWVjLWE0NzEtY2E3N2MyMjZmZjFiIiwicyI6Ik16SmlNVE15TURFdE16bG1OeTAwTUdVekxUZzVPV010T0dZNU1tWmxNVFl5TW1KaSJ9
