1233bd4ff315eb24850bc7214726263eb19c7b8e
EKO HENDRATNO <eko.hendratno@gmail.com>
2025-05-05 14:33:58 +0700

    Create tunnel-smkn1cdp.sh

---
+++ tunnel-smkn1cdp.sh
@@ -0,0 +1,5 @@
+curl -L --output cloudflared.deb https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64.deb && 
+
+dpkg -i cloudflared.deb && 
+
+sudo cloudflared service install eyJhIjoiYWFiMDFmNjEyNDQ4MjJhODYyOTZhMDRlYmU0ODEyM2QiLCJ0IjoiYjNhMTIwMmQtMGExNC00OWJkLWIyMjQtYjQ5ZTZjZGVlNTdjIiwicyI6IlptWmpPV05sT1RRdE1UTmtPQzAwWkdKbUxUaGlZV1F0T0dSbU1EUTFaalJqTnpObCJ9
